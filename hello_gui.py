0df21bd2d5ecaaed07831e4641d27fba16986569
Kelly Netterville <36lima@gmail.com>
2024-03-05 14:54:40 -0600

    Add button and entry widgets

--- hello_gui.py
+++ hello_gui.py
@@ -4,4 +4,16 @@ window = tk.Tk()
 greeting = tk.Label(text = "Hello, Tkinter")
 greeting.pack()
 
+button = tk.Button(
+    text="Click Me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+button.pack()
+
+entry = tk.Entry(fg="yellow", bg="blue", width=50)
+entry.pack()
+
 window.mainloop()
